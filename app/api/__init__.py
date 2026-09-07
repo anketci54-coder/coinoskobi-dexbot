586eee6cb9088ebc76c93b15ac0ad7142ca0cb35
anketci54-coder <anketci54@gmail.com>
2026-09-07 22:12:02 +0300

    Register live paper portfolio mark readmodel

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -4,6 +4,7 @@ from . import panel as _panel
 from .panel_acceptance import register_panel_acceptance_routes
 from .panel_display_names import enrich_universe_display_names
 from .panel_manual_paper_v2 import register_manual_paper_routes_v2
+from .panel_portfolio_marks_v2 import register_portfolio_marks_v2
 from .panel_provider_health import provider_health_snapshot, register_provider_health_route
 from .panel_universe import universe_panel_payload
 from .panel_watch_summary import register_watch_summary_route
@@ -19,6 +20,12 @@ def api_universe_panel():
 register_provider_health_route(_panel.app)
 
 register_manual_paper_routes_v2(
+    _panel.app,
+    paper_db=_panel.PAPER_DB,
+    cache_db=_panel.CACHE_DB,
+)
+
+register_portfolio_marks_v2(
     _panel.app,
     paper_db=_panel.PAPER_DB,
     cache_db=_panel.CACHE_DB,

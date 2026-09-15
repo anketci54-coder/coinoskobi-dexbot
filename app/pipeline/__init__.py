cfce828446c6f51cc9e94a6236768c18928f4842
anketci54-coder <anketci54@gmail.com>
2026-09-15 09:53:04 +0300

    Revert tx-origin warmup package hook

--- app/pipeline/__init__.py
+++ app/pipeline/__init__.py
@@ -1,16 +1,1 @@
 # pipeline package
-
-from app.pipeline.tx_origin_readiness_gate import (
-    install_pipeline_tx_origin_readiness_gate,
-)
-
-
-def _install_pipeline_origin_gate():
-    from app.pipeline.engine import PipelineEngine
-
-    install_pipeline_tx_origin_readiness_gate(
-        PipelineEngine
-    )
-
-
-_install_pipeline_origin_gate()

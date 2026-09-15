a5c770aa95b0b792d5860ec534ad487887350549
anketci54-coder <anketci54@gmail.com>
2026-09-15 09:28:32 +0300

    Bind origin readiness only to Pipeline warmup

--- app/pipeline/__init__.py
+++ app/pipeline/__init__.py
@@ -1,8 +1,16 @@
 # pipeline package
 
 from app.pipeline.tx_origin_readiness_gate import (
-    install_tx_origin_readiness_gate,
+    install_pipeline_tx_origin_readiness_gate,
 )
 
 
-install_tx_origin_readiness_gate()
+def _install_pipeline_origin_gate():
+    from app.pipeline.engine import Pipeline
+
+    install_pipeline_tx_origin_readiness_gate(
+        Pipeline
+    )
+
+
+_install_pipeline_origin_gate()

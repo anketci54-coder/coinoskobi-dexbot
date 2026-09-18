38fb8ff6a1bc24956a4b1d6fb5fbf3b3c910966c
anketci54-coder <anketci54@gmail.com>
2026-09-18 21:29:39 +0300

    Update runtime history tests for explicit provenance

--- tests/test_paper_math_state_persistence.py
+++ tests/test_paper_math_state_persistence.py
@@ -262,6 +262,7 @@ def test_runtime_math_price_history_isolated_by_pool():
                 1.0,
                 1.1,
             ],
+            price_series_source="PAIR_RUNTIME_ONCHAIN",
             exit_evidence={},
             lp_evidence={},
             market_context={},
@@ -276,6 +277,7 @@ def test_runtime_math_price_history_isolated_by_pool():
                 10.0,
                 11.0,
             ],
+            price_series_source="PAIR_RUNTIME_ONCHAIN",
             exit_evidence={},
             lp_evidence={},
             market_context={},
@@ -299,13 +301,13 @@ def test_runtime_math_price_history_isolated_by_pool():
         assert (
             "0xabc",
             "0xpool1",
-            "PAIR_ONCHAIN",
+            "PAIR_RUNTIME_ONCHAIN",
         ) in engine_module._RUNTIME_PRICE_HISTORY
 
         assert (
             "0xabc",
             "0xpool2",
-            "PAIR_ONCHAIN",
+            "PAIR_RUNTIME_ONCHAIN",
         ) in engine_module._RUNTIME_PRICE_HISTORY
 
     finally:
@@ -331,6 +333,7 @@ def test_runtime_math_history_isolates_cache_from_pair_onchain_source():
         **common,
         price=5.0e-5,
         upstream_price_series=[],
+        price_series_source="TOKEN_CACHE",
     )
 
     assert cache_result["price_series"] == [
@@ -347,6 +350,7 @@ def test_runtime_math_history_isolates_cache_from_pair_onchain_source():
         **common,
         price=2.42e-9,
         upstream_price_series=onchain_series,
+        price_series_source="PAIR_RUNTIME_ONCHAIN",
     )
 
     assert onchain_result["price_series"] == (
@@ -372,7 +376,7 @@ def test_runtime_math_history_isolates_cache_from_pair_onchain_source():
     assert (
         "0xtoken",
         "0xpool",
-        "PAIR_ONCHAIN",
+        "PAIR_RUNTIME_ONCHAIN",
     ) in keys
 
 

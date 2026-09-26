5387ca60c78c2dc745617dd3b9038cc8fbdefef9
anketci54-coder <anketci54@gmail.com>
2026-09-26 12:48:13 +0300

    Use USDT units in Phase15h BUY fixtures

--- tests/test_phase15_drift_composition.py
+++ tests/test_phase15_drift_composition.py
@@ -230,7 +230,7 @@ def test_phase15c_carries_phase15h_evidence_without_unit_laundering():
                     "chain_id": 56,
                 },
                 "received_token_raw": 2000,
-                "execution_price_native_per_token": 0.0005,
+                "execution_price_usdt_per_token": 0.0005,
                 "gas_used": 120000,
                 "effective_gas_price": 1_000_000_000,
                 "execution_gas_cost_wei": 120_000_000_000_000,
